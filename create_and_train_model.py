aa5b01aa2af2f9f968ee9f38f32d4ff570336230
김정수 <kjs20151240@gmail.com>
2024-09-19 12:10:06 +0900

    create | create_and_train_model.py
    모델을 생성하고 훈련하는 파일

---
+++ create_and_train_model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+def create_and_train_model(X_train, y_train, input_shape):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(X_train, y_train, epochs=5)
+    return model
